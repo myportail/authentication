e548ff03c9689fbb177f6b0edaee0f100eaebc1e
dannyt <cobreti_@hotmail.com>
2020-02-09 10:40:23 -0500

    updated script

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,6 @@
 #
 # run
-#    doctl kubernetes cluster kubeconfig save kubtest
+#    doctl kubernetes cluster kubeconfig save myportail
 #
 #   to init kubectl config
 #
@@ -40,6 +40,18 @@ resource "digitalocean_kubernetes_cluster" "myportail" {
 data "digitalocean_droplet" "myportail" {
   name = digitalocean_kubernetes_cluster.myportail.node_pool[0].nodes[0].name
 }
+
+# resource "digitalocean_volume" "storage" {
+#   region                  = "tor1"
+#   name                    = "dbstorage"
+#   size                    = 1
+#   description             = "mariadb volume"
+# }
+
+# resource "digitalocean_volume_attachment" "dbattachment" {
+#   droplet_id = data.digitalocean_droplet.myportail.id
+#   volume_id  = digitalocean_volume.storage.id
+# }
 
 output "kub_server_name" {
    value = digitalocean_kubernetes_cluster.myportail.name
