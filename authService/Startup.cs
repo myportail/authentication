fb5298e53112565437bca13244c2b20091283a64
dannyt <cobreti_@hotmail.com>
2020-04-03 21:35:51 -0400

    added multiple endpoint : http and htpps

--- authService/Startup.cs
+++ authService/Startup.cs
@@ -112,7 +112,6 @@ namespace authService
                 app.UseStaticFiles();
 
                 app.UseHsts();
-                app.UseHttpsRedirection();
                 app.UseRouting();
                 app.UseAuthorization();
                 app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
