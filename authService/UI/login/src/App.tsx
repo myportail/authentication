69944abc8a0b32109d85c96890aab1e10d266f9f
dannyt <cobreti_@hotmail.com>
2020-04-17 14:40:47 -0400

    changed css for scss

--- authService/UI/login/src/App.tsx
+++ authService/UI/login/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import ConfigService from "./services/configService";
 import Login from "./components/login";
 
