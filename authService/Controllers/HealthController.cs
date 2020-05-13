afe92b368c1d1b895c88fd9fb2acad4d7e10037f
dannyt <cobreti_@hotmail.com>
2020-05-13 14:55:52 -0400

    added health controller

---
+++ authService/Controllers/HealthController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using authService.Contexts;
+using authService.Models.Api.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace authService.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        private UserContext UserContext { get; }
+        
+        public HealthController(UserContext userContext)
+        {
+            UserContext = userContext;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> HealthStatus()
+        {
+            var status = new HealthStatus()
+            {
+                DatabaseConnection = await CheckDatabaseConnection()
+            };
+
+            return Ok(status);
+        }
+
+        private async Task<bool> CheckDatabaseConnection()
+        {
+            try
+            {
+                await UserContext.Users.FirstAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ authService/Models/Api/Results/HealthStatus.cs
@@ -0,0 +1,7 @@
+namespace authService.Models.Api.Results
+{
+    public class HealthStatus
+    {
+        public bool DatabaseConnection { get; set; }
+    }
+}
