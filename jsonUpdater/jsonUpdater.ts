47e0152eade2bdcb1152bc1a3ad6b872fba74715
dannyt <cobreti_@hotmail.com>
2020-05-17 19:48:22 -0400

    added json update project

---
+++ jsonUpdater/jsonUpdater.ts
@@ -0,0 +1,11 @@
+﻿import * as yargs from 'yargs';
+import {Argv} from 'yargs';
+
+let argv = yargs
+    .command('update', 'update json content', (yargs: Argv) => {
+        return yargs.option('file', {
+            describe: 'file to update'
+        });
+    }).argv;
+
+console.log(argv);
